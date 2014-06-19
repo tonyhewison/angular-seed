cd5e3081147a9f4a797770ed113d1f2a41e46217
Tony Hewison <mail@hewison.net>
2014-06-19 14:02:25 +0100

    Protractor temporary testing fix

--- test/protractor-conf.js
+++ test/protractor-conf.js
@@ -5,6 +5,20 @@ exports.config = {
     'e2e/*.js'
   ],
 
+  // There is selenium bug with selenium when using a standalone server.
+  // I thought it was necessary to use a specific version (2.9) of the
+  // chromedriver installed with Chrome browser version 35.0.1916.153 but,
+  // setting chromeOnly: true seems to workaround the problem by itself.
+  //
+  // Do not start a Selenium Standalone sever - only run this using chrome.
+  chromeOnly: true,
+  //
+  //
+  // The following line does not seem necessary.
+  // chromeDriver: 'path/to/my/chromedriver',
+  // e.g.
+//  chromeDriver: '/Users/tonyhewison/chromedriver/2.9/chromedriver',
+
   capabilities: {
     'browserName': 'chrome'
   },
